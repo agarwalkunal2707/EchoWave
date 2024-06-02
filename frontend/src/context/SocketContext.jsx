79738b6aa2aca5cdc483fa5e0c15d798a642cc46
Kunal Agarwal <agarwalkunal2707@gmail.com>
2024-06-03 01:24:52 +0530

    url add

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://chatwave-413f.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
